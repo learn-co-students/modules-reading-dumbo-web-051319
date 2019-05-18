8c541592c7793966b5cdabd9a91b93436eaf0d19
Tanuka16 <das.tanuka@yahoo.com>
2019-05-18 11:36:18 -0400

    modules_lab

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,7 @@
+#collect and buldle group of methods which can be used in different class methods
+module MetaDancing
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
